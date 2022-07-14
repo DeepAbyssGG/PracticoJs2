ba274a548e781bd56111d25a6fedd35df97d1ff7
Nicolas Gomez Marigliano <rodnicgm@gmail.com>
2022-07-14 16:14:15 -0300

    termineEjerciciosDel12al19

--- js/Ejercicio4.js
+++ js/Ejercicio4.js
@@ -1,7 +1,7 @@
 let suma = 0;
 do {
   let numero = parseInt(prompt("Introduce un numero"));
-  if ((numero = Number(numero))) {
+  if (numero = Number(numero)) {
     suma = suma + numero;
   } else if (numero != Number(numero)) {
     alert("El valor ingresado NO es un numero");
